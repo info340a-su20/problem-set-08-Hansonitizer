f45d5ef1aec76fcf860ba4ab78d606af1e50b793
Hanson Vuong <hanvuong@uw.edu>
2020-08-19 15:16:25 -0700

    Finished problemset

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -6,3 +6,63 @@ const EXAMPLE_SENATORS = [
 ];
 
 /* Your code goes here */
+
+export class App extends Component {
+  render() {
+    let senatorArray = this.props.senators;
+    let divs = (<div className="container">
+      <h1>US Senators 2019</h1>
+      <SenatorTable senators={senatorArray}/>
+      </div>);
+    return divs;
+  }
+}
+
+export class SenatorTable extends Component {
+  render() {
+    let colArray = ['Name', 'State', 'Phone', 'Twitter'];
+    let senatorsArray = this.props.senators;
+    let exSen = senatorsArray.map((rowObj) => {
+      let senRowArray = <SenatorRow key={rowObj.id.toString()} senator={rowObj} />;
+      return senRowArray;
+    });
+    let table = (<table className="table table-bordered">
+      <TableHeader cols={colArray} />
+      <tbody>{exSen}</tbody>
+      </table>);
+    return table;
+  }
+}
+
+export class TableHeader extends Component {
+  render() {
+    let columns = this.props.cols;
+    let columnArray = columns.map((item) => {
+      let transformed = <th key={item}>{item}</th>;
+      return transformed;
+    });
+    let tablehead = <thead><tr>
+      {columnArray}
+      </tr></thead>;
+    return tablehead;
+  }
+}
+
+export class SenatorRow extends Component {
+  render() {
+    let senators = this.props.senator; // Senator object
+    let senName = <td key={senators.name}>{senators.name}</td>;
+    let senSP = <td key={senators.state}>{senators.party.substring(0, 1)} - {senators.state}</td>;
+    let phoneRef = 'tel:' + senators.phone;
+    let senPhone = <td key={senators.phone}><a href={phoneRef}>{senators.phone}</a></td>;
+    let twitLink = 'https://twitter.com/' + senators.twitter;
+    let senTwitt = <td key={senators.twitter}><a href={twitLink}>@{senators.twitter}</a></td>
+    let tableContent = (<tr>
+      {senName}
+      {senSP}
+      {senPhone}
+      {senTwitt}
+      </tr>);
+    return tableContent;
+  }
+}
--- problem-b/src/App.js
+++ problem-b/src/App.js
@@ -1,9 +1,123 @@
 import React, { Component } from 'react'; //import React Component
+import _ from 'lodash';
+import './style.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {pets: this.props.pets};
+  }
+  adopt = (name) => {
+    this.setState((currentState) => {
+      let correctPet = _.find(currentState.pets, {'name': name});
+      correctPet.adopted = true; 
+      return correctPet;
+    })
+  }
   render() {
+    let petsObj = this.state.pets;
+    let grouped = _.groupBy(petsObj, 'breed');
+    let petsArray = Object.keys(grouped);
     return (
-      <div>Replace this with your content!</div>
+      <div>
+        <header className="jumbotron jumbotron-fluid py-4">
+        <div className="container">
+          <h1>Adopt a Pet</h1>
+        </div>
+      </header>
+      
+      <main className="container">
+        <div className="row">
+          <div id="navs" className="col-3">
+            <BreedNav breeds={petsArray}/>
+            <AboutNav />
+          </div>
+          <div id="petList" className="col-9">
+            <PetList pets={petsObj} adoptCallback={this.adopt}/>
+          </div>
+        </div>
+      </main>
+
+      <footer className="container">
+        <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
+      </footer>
+    </div>
+    );
+  }
+}
+
+class AboutNav extends Component {
+  render() {
+    return(
+      <nav id="aboutLinks">
+        <h2>About</h2>
+        <ul className="list-unstyled">
+          <li><a href="#/">How to Adopt</a></li>
+          <li><a href="#/">Volunteering</a></li>
+          <li><a href="#/">Events</a></li>
+          <li><a href="#/">Donate</a></li>
+          <li><a href="#/">About Us</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class BreedNav extends Component {
+  render() {
+    let breeds = this.props.breeds;
+    let breedArray = breeds.map((item) => {
+      let listArray = <li key={item}><a href="">{item}</a></li>;
+      return listArray;
+    });
+    return(
+      <nav id="breedLinks">
+        <h2>Pick a Breed</h2>
+        <ul class="list-unstyled">
+          {breedArray}
+        </ul>            
+      </nav>
+    );
+  }
+}
+
+class PetCard extends Component {
+  handleClick = () => {
+    this.props.adoptCallback(this.props.dogs.name);
+  }
+  render() {
+    let petObject = this.props.dogs;
+    let displayedName = petObject.name;
+    if (petObject.adopted == true) {
+      displayedName = displayedName + " (Adopted)";
+    }
+    let card = 
+      (<div className="card" onClick={this.handleClick}>
+        <img className="card-img-top" src={petObject.img} alt={petObject.name} />
+          <div className="card-body">
+            <h3 className="card-title">{displayedName}</h3>
+            <p className="card-text">{petObject.sex} {petObject.breed}</p>
+          </div>
+      </div>);
+    return card; 
+  }
+}
+
+class PetList extends Component {
+  render() {
+    let petArrObj = this.props.pets;
+    let callback = this.props.adoptCallback;
+    let petCardArr = petArrObj.map((petObj) => {
+      let cards = <PetCard dogs={petObj} adoptCallback={callback} />;
+      return cards;
+    });
+    return(
+      <div>
+      <h2>Dogs for Adoption</h2>
+      <div class="card-deck">
+        {petCardArr}
+      </div>
+      </div>
     );
   }
 }
